0d730a9415d63156a0559e6858b54755b553b1c3
Camillarhi <rhitanene@gmail.com>
2022-06-14 11:21:04 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   // await app.listen(8000);
-  await app.listen(parseInt( '0.0.0.0') || 3000);
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
  
